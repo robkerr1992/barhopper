b478d8c776d623fa155f88c6a43dd8c14425c3f6
Robert Kerr <rob@robertkerr.me>
2019-02-24 20:10:42 -0600

    views for version 1.0

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are home!
                 </div>
             </div>
         </div>
